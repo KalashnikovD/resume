d94427c04c4a108cdabe2b0c8a3b8b1c9a1f061a
KalashnikovD <kalashnikov.dmytro@gmail.com>
2023-05-15 16:10:26 +0200

    Add webp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,20 +3,31 @@ import './header.scss';
 import HeaderContacts from './HeaderContacts.jsx';
 import Navigation from './nav';
 import { useDataContext } from '../provider/Provider';
+import ImgWithFallback from '../imgWithFallback/ImgWithFallback';
+import { useState } from 'react';
 
 
 function Header(){
     const {data} = useDataContext();
+    const [headerPosFix, setHeaderPosFix] = useState(false);
+    
+   
+
     return(
         <>
             <HeaderContacts/>
-            <header>               
-                <div className='header-logo'><Link to={"/"}><img src={data.logo.src} alt="={data.logo.alt}" /></Link></div>
+            <header className={headerPosFix ? "headerFixed" : ""}>               
+                <div className='header-logo'>
+                    <Link to={"/"}>
+                        <ImgWithFallback {...data.logo.image}/>
+                    </Link>
+                </div>
                 <Navigation/>
             </header>
         </>
 
     )
 }
+
 
 export default Header;
